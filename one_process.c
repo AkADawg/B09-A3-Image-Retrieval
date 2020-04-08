c97554ec460ba4ec072578b4073ddbea4d85e264
AkADawg <56404184+AkADawg@users.noreply.github.com>
2020-04-08 15:49:54 +0000

    task 2 completed
    checked on uni server. all completed no warnings/ errors

--- one_process.c
+++ one_process.c
@@ -9,72 +9,90 @@
 #include "worker.h"
 
 int main(int argc, char **argv) {
-	
+
 	char ch;
 	char path[PATHLENGTH];
 	char *startdir = ".";
-        char *image_file = NULL;
+	char *image_file = NULL;
 
-	while((ch = getopt(argc, argv, "d:")) != -1) {
+
+	while ((ch = getopt(argc, argv, "d:")) != -1) {
 		switch (ch) {
-			case 'd':
+		case 'd':
 			startdir = optarg;
 			break;
-			default:
+		default:
 			fprintf(stderr, "Usage: queryone [-d DIRECTORY_NAME] FILE_NAME\n");
 			exit(1);
 		}
 	}
 
-        if (optind != argc-1) {
-	     fprintf(stderr, "Usage: queryone [-d DIRECTORY_NAME] FILE_NAME\n");
-        } else
-             image_file = argv[optind];
+	if (optind != argc - 1) {
+		fprintf(stderr, "Usage: queryone [-d DIRECTORY_NAME] FILE_NAME\n");
+	}
+	else
+		image_file = argv[optind];
 
 	// Open the directory provided by the user (or current working directory)
-	
+
 	DIR *dirp;
-	if((dirp = opendir(startdir)) == NULL) {
+	if ((dirp = opendir(startdir)) == NULL) {
 		perror("opendir");
 		exit(1);
-	} 
-	
+	}
+
 	/* For each entry in the directory, eliminate . and .., and check
 	* to make sure that the entry is a directory, then call run_worker
 	* to process the image files contained in the directory.
 	*/
-		
+
 	struct dirent *dp;
-        CompRecord CRec;
+	CompRecord CRec;
 
-	while((dp = readdir(dirp)) != NULL) {
+	while ((dp = readdir(dirp)) != NULL) {
+		//	printf("how many times am i called?\n");
 
-		if(strcmp(dp->d_name, ".") == 0 || 
-		   strcmp(dp->d_name, "..") == 0 ||
-		   strcmp(dp->d_name, ".svn") == 0){
+		if (strcmp(dp->d_name, ".") == 0 ||
+			strcmp(dp->d_name, "..") == 0 ||
+			strcmp(dp->d_name, ".svn") == 0) {
 			continue;
 		}
+
+		//printf("how many times am i called2?\n");
+
+
 		strncpy(path, startdir, PATHLENGTH);
 		strncat(path, "/", PATHLENGTH - strlen(path) - 1);
 		strncat(path, dp->d_name, PATHLENGTH - strlen(path) - 1);
 
 		struct stat sbuf;
-		if(stat(path, &sbuf) == -1) {
+		if (stat(path, &sbuf) == -1) {
 			//This should only fail if we got the path wrong
 			// or we don't have permissions on this entry.
 			perror("stat");
 			exit(1);
-		} 
-
+		}
+		//printf("this is path: %s", path);
 		// Only call process_dir if it is a directory
 		// Otherwise ignore it.
-		if(S_ISDIR(sbuf.st_mode)) {
-                        printf("Processing all images in directory: %s \n", path);
+		if (S_ISDIR(sbuf.st_mode)) {
+			printf("Processing all images in directory: %s \n", path);
+			//printf("this is start directory: %s and this is dp->d_name : %s \n", startdir, dp->d_name);
+			//char* name = strcat(startdir, dp->d_name);
+			//printf("what is startdir: %s \n", name); 
+			/*CompRecord temp = process_dir(path, read_image(image_file), STDOUT_FILENO);
+			printf("Comparing %s with %d and %s with %d\n", CRec.filename, CRec.distance, temp.filename, temp.distance);
+			if (CRec.distance > temp.distance) {
+				CRec = temp;
+			}*/
+			CompRecord temp = process_dir(path, read_image(image_file), STDOUT_FILENO);
+			if (CRec.distance > temp.distance) {
+				CRec = temp;
+			}
 		}
-		
 	}
 
-        printf("The most similar image is %s with a distance of %f\n", CRec.filename, CRec.distance);
-	
+	printf("The most similar image is %s with a distance of %f\n", CRec.filename, CRec.distance);
+
 	return 0;
 }
---
+++ w.c
@@ -0,0 +1,237 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <math.h>
+#include <float.h>
+#include "worker.h"
+
+#define LINE_LENGTH 70
+
+
+/*
+* Read an image from a file and create a corresponding
+* image struct
+*/
+
+Image* read_image(char *filename)
+{
+	FILE *file;
+	int error;
+	char* end;
+	char line[LINE_LENGTH + 1];
+	int word;
+
+	Image *img = (Image*)malloc(sizeof(Image));
+
+	/**Error checking for successful memory allocation*/
+	if (!img) {
+		fprintf(stderr, "Unable to allocate memory\n");
+		exit(1);
+	}
+
+
+	/*Open File and check for errors*/
+	file = fopen(filename, "r");
+
+	if (file == NULL) {
+		fprintf(stderr, "Error opening file\n");
+		exit(1);
+	}
+
+	/*Scan through file to get required information and store in struct*/
+	fscanf(file, "%s", line);
+	if (strcmp(line, "P3") != 0) {
+		exit(1);
+	}
+
+	// Read and store width
+	fscanf(file, "%s", line);
+	int width = strtol(line, &end, 10);
+	img->width = width;
+	//printf("width: %d\n", width);
+
+	// Read and store height
+	fscanf(file, "%s", line);
+	int height = strtol(line, &end, 10);
+	img->height = height;
+	//	printf("height: %d\n", height);
+
+		// Read and store max value
+	fscanf(file, "%s", line);
+	int maxValue = strtol(line, &end, 10);
+	img->max_value = maxValue;
+	//	printf("max value: %d\n", maxValue);
+
+		/*Allocate memory for pixels and check for errors*/
+	img->p = (Pixel*)malloc(width * height * sizeof(Pixel));
+
+	if (!img->p) {
+		fprintf(stderr, "Unable to allocate memory\n");
+	}
+
+	/*Scan through file and store pixel information*/
+	for (int i = 0; i < width *height; i++) {
+
+		//Read and store red pixel
+		fscanf(file, "%d", &word);
+		img->p[i].red = word;
+		//	printf("red: %d\n", word);
+
+			//Read and store green pixel
+		fscanf(file, "%d", &word);
+		img->p[i].green = word;
+		//	printf("green: %d\n", word);
+
+
+			//Read and store blue pixel
+		fscanf(file, "%d", &word);
+		img->p[i].blue = word;
+		//	printf("blue: %d\n", word);
+
+	}
+
+	/*Close file and check for errors*/
+	error = fclose(file);
+
+	if (error != 0) {
+		fprintf(stderr, "Error closing file\n");
+		exit(1);
+	}
+
+	//free(img->p);
+	//free(img);
+	return img;
+}
+
+/*
+* Print an image based on the provided Image struct
+*/
+
+void print_image(Image *img) {
+	printf("P3\n");
+	printf("%d %d\n", img->width, img->height);
+	printf("%d\n", img->max_value);
+
+	for (int i = 0; i < img->width*img->height; i++)
+		printf("%d %d %d  ", img->p[i].red, img->p[i].green, img->p[i].blue);
+	printf("\n");
+}
+
+/*
+* Compute the Euclidian distance between two pixels
+*/
+float eucl_distance(Pixel p1, Pixel p2) {
+	return sqrt(pow(p1.red - p2.red, 2) + pow(p1.blue - p2.blue, 2) + pow(p1.green - p2.green, 2));
+}
+
+/*
+* Compute the average Euclidian distance between the pixels
+* in the image provided by img1 and the image contained in
+* the file filename
+*/
+
+float compare_images(Image *img1, char *filename)
+{
+	FILE* file;
+	int euclD = 0;
+
+	Image *img2 = read_image(filename);
+
+	//Check if dimensions are the same
+	if (img1->height != img2->height || img1->width != img2->width) {
+		return FLT_MAX;
+	}
+
+
+	/*Calculate euclidean distance for both images*/
+	for (int i = 0; i < img1->width *img1->height; i++) {
+		euclD += eucl_distance(img1->p[i], img2->p[i]);
+	}
+
+	printf("eucli: %d\n", euclD);
+	return euclD;
+}
+
+/* process all files in one directory and find most similar image among them
+* - open the directory and find all files in it
+* - for each file read the image in it
+* - compare the image read to the image passed as parameter
+* - keep track of the image that is most similar
+* - write a struct CompRecord with the info for the most similar image to out_fd
+*/
+CompRecord process_dir(char *dirname, Image *img, int out_fd) {
+
+	CompRecord CRec;
+	struct dirent *dp;
+	char path[PATHLENGTH];
+	struct stat sbuf;
+	DIR *dirp;
+	float mostSimilar = FLT_MAX;
+	float result;
+	char filename[PATHLENGTH];
+
+	/*Open directory and check for error*/
+	dirp = opendir(dirname);
+	if ((dirp == NULL)) {
+		fprintf(stderr, "Unable to open directory");
+		exit(1);
+	}
+
+
+	/*Find all the files in the directory*/
+	while ((dp = readdir(dirp)) != NULL) {
+
+		if (strcmp(dp->d_name, ".") == 0 ||
+			strcmp(dp->d_name, "..") == 0 ||
+			strcmp(dp->d_name, ".svn") == 0) {
+			continue;
+		}
+		strncpy(path, dirname, PATHLENGTH);
+		strncat(path, "/", PATHLENGTH - strlen(path) - 1);
+		strncat(path, dp->d_name, PATHLENGTH - strlen(path) - 1);
+
+
+		if (stat(path, &sbuf) == -1) {
+			fprintf(stderr, "stat erorr");
+			exit(1);
+		}
+
+		if (S_ISREG(sbuf.st_mode)) {
+			printf("%s\n", dp->d_name);
+			result = compare_images(img, path);
+			printf("this is result %f and this is most similar %f", result, mostSimilar);
+		}
+
+
+		if (result < mostSimilar) {
+			printf("result: %f\n", result);
+			mostSimilar = result;
+			strcpy(filename, path);
+
+		}
+	}
+
+	printf("most similar filename is: %s\n", filename);
+	strcpy(CRec.filename, filename);
+	CRec.distance = mostSimilar;
+
+	if (closedir(dirp) == -1) {
+		fprintf(stderr, "Error closing directory\n");
+	};
+
+	return CRec;
+}
+
+/*
+int main() {
+
+	printf(compare_images(read_image("sample.ppm"), "top10.txt"));
+	//print_image(read_image("sample.ppm"));
+	//read_image("hello.txt");
+	return 0;
+}*/
+
